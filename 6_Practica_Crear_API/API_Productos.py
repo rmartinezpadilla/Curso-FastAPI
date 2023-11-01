21fe3b5f3540c7c9caf9d3acb590be52c4678164
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-11-01 15:37:08 -0500

    buscando elemento por el ID

--- 6_Practica_Crear_API/API_Productos.py
+++ 6_Practica_Crear_API/API_Productos.py
@@ -1,6 +1,8 @@
 #importar fastapi
 from fastapi import FastAPI
 from models.producto  import Productos
+#importamos uuid para los id automaticos
+from uuid import uuid4 as uuid
 
 app= FastAPI()
 
@@ -10,11 +12,19 @@ lista_productos = []
 def index():
     return {'Bienvenidos a la API de productos'}
 
-@app.get('/producto')
-def obtener_producto():
+@app.get('/productos')
+def obtener_todos_los_productos():
     return lista_productos
 
-@app.post('/producto')
+@app.get('/productos/{producto_id}')
+def obtener_producto_por_id(producto_id: str):
+    for p in lista_productos:
+        if p.id_producto == producto_id:
+            return p
+    return {'Mensaje': f'Producto con el id {producto_id} no encontrado'}
+
+@app.post('/crearproducto')
 def crear_producto(producto: Productos):
+    producto.id_producto = str(uuid())
     lista_productos.append(producto)
-    return 'Producto creado y agregado correctamente!'
+    return {'Mensaje': f'Producto {producto.nombre_producto}  creado y agregado correctamente!'}
