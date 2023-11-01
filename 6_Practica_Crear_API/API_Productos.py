297ffe38a8d5fff3b9378d2ed6f5bde94c9df451
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-11-01 15:51:05 -0500

    Mejorando la consulta del producto por ID usando filter

--- 6_Practica_Crear_API/API_Productos.py
+++ 6_Practica_Crear_API/API_Productos.py
@@ -1,8 +1,10 @@
-#importar fastapi
-from fastapi import FastAPI
+#importar fastapi HTTP exception
+from fastapi import FastAPI, HTTPException
 from models.producto  import Productos
 #importamos uuid para los id automaticos
 from uuid import uuid4 as uuid
+#importamos las excepciones HTTP
+
 
 app= FastAPI()
 
@@ -18,10 +20,19 @@ def obtener_todos_los_productos():
 
 @app.get('/productos/{producto_id}')
 def obtener_producto_por_id(producto_id: str):
-    for p in lista_productos:
-        if p.id_producto == producto_id:
-            return p
-    return {'Mensaje': f'Producto con el id {producto_id} no encontrado'}
+    #forma tradicional
+    
+    # for p in lista_productos:
+    #     if p.id_producto == producto_id:
+    #         return p
+    
+    """usando lamba y mejorando el código"""
+    resultado = list(filter(lambda p : p.id_producto == producto_id, lista_productos))
+    if len(resultado) > 0:
+        return resultado[0]
+    
+    raise HTTPException(status_code=400, detail=f'Producto {producto_id} No encontrado!')
+    #return {'Mensaje': f'Producto con el id {producto_id} no encontrado'}
 
 @app.post('/crearproducto')
 def crear_producto(producto: Productos):
