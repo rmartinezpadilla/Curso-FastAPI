1f3ba770658a0f88ca9df9c48a8fc4a5c2d78adc
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-11-02 14:47:50 -0500

    ejercicio 2 metodos GET atributo por path o por query

--- 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_1/FastAPI/main.py
+++ 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_1/FastAPI/main.py
@@ -4,10 +4,16 @@ from fastapi import FastAPI
 #instancia de fastiAPI
 app = FastAPI()
 
+#Iniciar el servidor: uvicorn archivo:app --reload
+#Detener el servidor: CTRL + C
+
 @app.get('/')
 async def root():
     return {'Mensaje': 'Hola FastAPI'}
 
-@app.get('/info')
-async def root():
-    return {'URL curso': 'https://ejemplo.com'}
+@app.get('/url')
+async def url():
+    return {'URL': 'https://ejemplo.com'}
+
+#documentacion Swagger: http://127.0.0.1:8000/docs
+#documentacion Redocly: http://127.0.0.1:8000/redoc
---
+++ 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_2/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from model.usuario import Usuario as usr
+
+app = FastAPI()
+
+#Iniciar el servidor: uvicorn archivo:app --reload
+#Detener el servidor: CTRL + C
+
+user_list = [usr(cedula=987, nombres = "Juan", apellidos= "Marín", direccion= "Pradera"),
+             usr(cedula=654, nombres = "Elías", apellidos= "López", direccion= "Canta claro"),
+             usr(cedula=123, nombres = "María", apellidos= "Garcés", direccion= "Mogambo"),
+             usr(cedula=951, nombres = "Ana", apellidos= "Gómez", direccion= "P5")             
+             ]
+
+user_list_II = []
+
+@app.get('/')
+async def root():       
+   return {'Mensaje':'Bienvenidos a esta API de practica'}
+
+@app.get('/users')
+async def users():    
+   # return {usr(cedula = 98521, nombres = "Juan", apellidos = "PEREZ", direccion="Pradera")}
+   return user_list
+
+# Pedir el dato por el path
+@app.get('/user/{user_id}')
+async def user(ced : int):    
+   # return {usr(cedula = 98521, nombres = "Juan", apellidos = "PEREZ", direccion="Pradera")}
+   #funcion de orde superior
+#    resultado = filter(lambda usr: usr.cedula == ced, user_list)
+#    try:
+#     return list(resultado)[0]
+#    except:
+#     return {'La lista está vacía'}
+    return serch_user(ced)
+
+# Pedir el dato por una query    
+@app.get('/userquery/')
+async def user(ced : int):    
+   # return {usr(cedula = 98521, nombres = "Juan", apellidos = "PEREZ", direccion="Pradera")}
+   #funcion de orde superior
+#    resultado = filter(lambda usr: usr.cedula == ced, user_list)
+#    try:
+#     return list(resultado)[0]
+#    except:
+#     return {'Mensaje':'No se ha encontrado al usuario'}
+    return serch_user(ced)
+
+#tambien se pueden crear métodos que se usen en diferentes petidiciones
+#creamos un método
+
+def serch_user(ced: int):
+    #funcion de orde superior
+   resultado = filter(lambda usr: usr.cedula == ced, user_list)
+   try:
+    return list(resultado)[0]
+   except:
+    return {'Mensaje':'No se ha encontrado al usuario'}
