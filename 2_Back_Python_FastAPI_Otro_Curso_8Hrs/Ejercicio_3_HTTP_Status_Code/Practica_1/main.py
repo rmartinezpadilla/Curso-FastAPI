493b48ffa49b71f9d5b01c7701a14d3d232fbd17
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-11-03 14:14:21 -0500

    mejorando respuesta de endpoint

--- 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_3_HTTP_Status_Code/Practica_1/main.py
+++ 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_3_HTTP_Status_Code/Practica_1/main.py
@@ -19,19 +19,27 @@ async def get_phones():
     if not bool(list_phone):
         return {'Mensaje':'Lista vacía'}
     else:        
-        return {'Telefonos' : list_phone}
+        #return {'Telefonos' : list_phone}
+        return list_phone
 
-@app.post('/phone/', status_code=201)
+
+# ----------------- IMPORTANTE -----------------
+#     Agregamos dentro del  metodo add_phone el parametro response_model= '' 
+#     para decirle al método en caso de que todo vaya bien responda dicho objeto
+#     podemos decirle al metodo que va a recibir si todo va bien y que va a devolver 
+    
+@app.post('/phone/',status_code=201, response_model=phone)
 async def add_phone(cel : phone):
      #primero comprobamos si el telefono existe en la lista
    if type(serch_phone(cel.id)) == phone:
-       # Creamo una respuesta al error de respues 204
+       # Creamos una respuesta al error de respues 204
        # return HTTPException(status_code=204, detail=f'El telfono {cel.marca} con id {cel.id} ya existe en la lista')
-       raise HTTPException(status_code=404, detail=f'El telfono {cel.marca} con id {cel.id} ya existe en la lista')
+       #controlamos el mensaje de error de la respuesta
+       raise HTTPException(status_code=404, detail=f'El telefono {cel.marca} con id {cel.id} ya existe en la lista')
       #return {'Error':f'El telfono {cel.marca} con id {cel.id} ya existe en la lista'}
    else:
       list_phone.append(cel)
-      return {'Mensaje' :  cel}
+      return cel
       
 
 #tambien se pueden crear métodos que se usen en diferentes petidiciones
