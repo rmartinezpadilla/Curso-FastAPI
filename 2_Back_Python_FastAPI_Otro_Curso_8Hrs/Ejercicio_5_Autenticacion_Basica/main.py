590c4786487c94d2b56de78d7fa960a20e8ff2d4
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-11-05 17:11:15 -0500

    Encriptacion con JWT

--- 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_5_Autenticacion_Basica/main.py
+++ 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_5_Autenticacion_Basica/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import products, users
+from routers import products, users, jwt_auth_users, basic_auth_users
 # importamos la clase Statics files para poder accerder a archivos dentro de nuestra API
 # estos recursos son estaticos
 from fastapi.staticfiles import StaticFiles
@@ -11,6 +11,8 @@ app  = FastAPI()
 #usamos la instancia de FastAPI "app" para llamar el Api de productos como una ruta
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(jwt_auth_users.router)
+app.include_router(basic_auth_users.router)
 
 # Vamos a llamar a la imagen, en recursos estaticos (archivos, documentos, etc)
 app.mount('/static', StaticFiles(directory='static'), name='static')
--- 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_5_Autenticacion_Basica/routers/jwt_auth_users.py
+++ 2_Back_Python_FastAPI_Otro_Curso_8Hrs/Ejercicio_5_Autenticacion_Basica/routers/jwt_auth_users.py
@@ -3,12 +3,21 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 # Importamos el modulo de seguridad de fastapi
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 1
+SECRET = '5b1638be027fe9f7904b6a1b55838674'
 
 router = APIRouter()
 
 # Creamos instancia del sistema de autenticacion estandar de fastAPI
 # le pasamos como paramtros el atributo tokenUrl = "login "---- Importante
 oauth2 = OAuth2PasswordBearer( tokenUrl='login')
+
+crypt = CryptContext(schemes=["bcrypt"])
 
 class Usuario(BaseModel):
     username : str
@@ -21,28 +30,28 @@ class Usuario(BaseModel):
 
 class User_bd(Usuario):
     password :  str
-    
+
 users_db = {
     'Juan' : {
         "username" : 'Juan',
         "full_name" : 'Juan Ospina',
         "email" : 'juan@hotmail.com',
         "disabled" : False,
-        'password' : '123456'
+        'password' : '$2a$12$dAf3E3bkv81Mfwcw2xD.y.XCifAkVOFkb1AyU0rLDywAUOhdpW.6i'
     },
     'Maria' : {
         "username" : 'Maria',
-        "full_name" : 'maria miranda',
+        "full_name" : 'maría miranda',
         "email" : 'maria@hotmail.com',
         "disabled" : True,
-        'password' : '987654'
+        'password' : '$2a$12$qznF18zyLLbyqKH9gETN7.Epa/ESPCtMgjKmy/94f07hbCXq6MZgm'
     },
     'Estevan' : {
-        "username" : 'estevan',
+        "username" : 'Estevan',
         "full_name" : 'Estevan Julio',
         "email" : 'estevan@hotmail.com',
         "disabled" : False,
-        'password' : '001122'
+        'password' : '$2a$12$fbMBNRnDr/zeYS9p6hwIqeitKusiMj6RAqjRm9uMmgdQZAyRIA/w.'
     }
 }
 
@@ -52,21 +61,8 @@ def searchuser_db(username : str):
 
 def search_user(username : str):
     if username in users_db:
-        return Usuario(**users_db[username])
-
-async def current_user(token : str = Depends(oauth2)):
-    user =  search_user(token)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
-                            detail='Credenciales de autenticación invalidas', 
-                            headers={'WWW-Authenticate' : 'Bearer'})
-    
-    if user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
-                            detail='Usuario inactivo')
-    return user
+        return Usuario(**users_db[username]) 
         
-
 #Implementamos la autenticacion   
 # Le pasamos como parametro en del path el mismo valor que le pusimos en la 
     # instancia de la clase OAuth2PasswordBearer, es decir, 'login'
@@ -79,13 +75,43 @@ async def login(form : OAuth2PasswordRequestForm = Depends()):
        
     user  = searchuser_db(form.username)
     
-    if not form.password == user.password:
+    crypt.verify(form.password, user.password)
+    
+    if not crypt:
         raise HTTPException(status_code=400, detail='Contraseña incorrecta')
     
-    return {'access_token' : user.username, 'token_type' : 'bearer'}
+    expire_token  = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
+    
+    acces_token = {
+        'sub' : user.username,
+        'exp' :expire_token
+    }   
+        
+    return {'access_token' : jwt.encode(acces_token, SECRET, algorithm=ALGORITHM), 'token_type' : 'bearer'}
+
+async def auth_user(token : str = Depends(oauth2)):    
+    try:
+        user_name =  jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get('sub')
+        
+        if user_name is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail='usuario vacio', 
+                headers={'WWW-Authenticate' : 'Bearer'})        
+    except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail='Credenciales de autenticación invalidas o token vencido', 
+                headers={'WWW-Authenticate' : 'Bearer'})
+    
+    return search_user(user_name)
+    
+
+async def current_user(user : Usuario = Depends(auth_user)):
+    
+    if user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail='Usuario inactivo')
+    return user
 
 @router.get('/users/me')
 async def me(user : Usuario = Depends(current_user)):
     return user
-        
-        #Video 4:54
